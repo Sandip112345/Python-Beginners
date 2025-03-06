8c756a4979a84ab894e056c9f2a44a663b4aa52b
Sandip112345 <sandip2211175@iimscollege.edu.np>
2025-03-06 14:30:22 +0545

    done mega project named jarvis as in iron man's assistant robotic ai

---
+++ Mega_Project_1/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "change" : "https://youtu.be/-WZfSSoaL8A?si=3xwD4ZppS8Pbj4wj",
+    "gang": "https://youtu.be/OkEwZ8cx-54?si=UJljXPpwojMUp_7r",
+    "mortals" : "https://youtu.be/yJg-Y5byMMw?si=xi0fjedky1AwyHia",
+    "heroes": "https://youtu.be/3nQNiWdeH2Q?si=rU7WsiU6m_GiNMT8"
+}
